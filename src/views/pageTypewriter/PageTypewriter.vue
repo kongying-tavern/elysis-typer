bbec6dda1f612f1c4f3af0a393acad41507fb9ba
Alex Fang <deeno@qq.com>
2023-02-28 22:48:26 +0800

    feat(font-picker): add font picker component

--- src/views/pageTypewriter/PageTypewriter.vue
+++ src/views/pageTypewriter/PageTypewriter.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FontPicker from "./components/FontPicker/FontPicker.vue";
+</script>
 
 <template>
   <main class="fixed top-0 bottom-0 left-0 right-0 flex flex-row">
-    <div class="flex-1" style="background-color: blue">font-picker</div>
+    <div class="flex-1">
+      <FontPicker></FontPicker>
+    </div>
     <div class="flex-3 flex flex-col">
       <div class="flex-1" style="background-color: orange">text-preview</div>
       <div class="flex-1" style="background-color: green">text-input</div>
