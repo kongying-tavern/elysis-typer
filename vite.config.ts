117e4ccd7329bca22cb6f4fc5e1f66eb7c41cd6d
Alex Fang <deeno@qq.com>
2023-04-04 23:47:58 +0800

    fix: update build path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { resolve } from "node:path";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
+const env = loadEnv(
+  process.env.NODE_ENV as string,
+  resolve(process.cwd(), "env"),
+  "VITE_"
+);
+
 export default defineConfig({
-  base: './',
+  base: env.VITE_DIST_PATH || "./",
   server: {
     host: "0.0.0.0",
     port: 9000,
