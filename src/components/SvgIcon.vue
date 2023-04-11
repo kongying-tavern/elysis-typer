bfd5e4310d050f2ef7e8ade5a6a331482006df7c
Alex Fang <deeno@qq.com>
2023-04-11 19:32:42 +0800

    fix(icon-resolve): fix icon path resolve

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import { computed, withDefaults } from "vue";
-
-interface Props {
-  iconBase?: string;
+defineProps<{
   iconSrc: string;
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  iconBase: import.meta.url,
-  iconSrc: "",
-});
-
-const iconUrl = computed(() => new URL(props.iconSrc, props.iconBase).href);
+}>();
 </script>
 
 <template>
-  <img :src="iconUrl" />
+  <img :src="iconSrc" />
 </template>
--- src/views/pageTypewriter/components/ConfigBar/ButtonClean.vue
+++ src/views/pageTypewriter/components/ConfigBar/ButtonClean.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { useTextInput } from "../../hooks";
 import ButtonBase from "./ButtonBase.vue";
+import SvgTrash from "./assets/trash.svg";
 
 const { clear } = useTextInput();
 </script>
 
 <template>
-  <ButtonBase icon-src="./assets/trash.svg" @click="clear()"></ButtonBase>
+  <ButtonBase :icon-src="SvgTrash" @click="clear()"></ButtonBase>
 </template>
