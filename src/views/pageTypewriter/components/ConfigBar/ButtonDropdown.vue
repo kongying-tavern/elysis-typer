6d09077f54b2bcab3c49f0e6621a0d174ff28504
Alex Fang <deeno@qq.com>
2023-04-13 16:20:00 +0800

    fix(button-dropdown): remove button dropdown arrow from popup

--- src/views/pageTypewriter/components/ConfigBar/ButtonDropdown.vue
+++ src/views/pageTypewriter/components/ConfigBar/ButtonDropdown.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+const popperOptions = {
+  modifiers: [
+    {
+      enabled: true,
+      phase: "write",
+      fn({ state }) {
+        const { arrow } = state.elements;
+        arrow.remove();
+      },
+    },
+  ],
+};
+</script>
+
 <template>
-  <el-dropdown placement="top-start" trigger="click">
+  <el-dropdown
+    placement="top-start"
+    trigger="click"
+    :popper-options="popperOptions"
+  >
     <slot></slot>
     <template #dropdown>
       <slot name="dropdown"></slot>
