776d3b6aa429d33f3e6e7751fba30aff7ccc23ff
Alex Fang <deeno@qq.com>
2023-05-06 04:00:22 +0800

    feat(input-area): add clear button to input area

--- src/views/pageTypewriter/components/Portrait/InputArea/InputArea.vue
+++ src/views/pageTypewriter/components/Portrait/InputArea/InputArea.vue
@@ -3,10 +3,13 @@ import { computed } from "vue";
 import { ConfigConvertDirectionEnum } from "@/shared";
 import CardBasic from "@/components/CardBasic.vue";
 import TextAreaBasic from "@/components/TextAreaBasic.vue";
+import SvgIcon from "@/components/SvgIcon.vue";
+import ImgClose from "../../../assets/close.svg";
 import { useConfig, useTextInput } from "../../../hooks";
+import variables from "./variables.module.scss";
 
 const { config } = useConfig();
-const { text } = useTextInput();
+const { text, clear } = useTextInput();
 
 const fontClass = computed(() => {
   if (config.value.convertDirection === ConfigConvertDirectionEnum.TO_ENG) {
@@ -19,15 +22,29 @@ const fontClass = computed(() => {
 </script>
 
 <template>
-  <CardBasic>
+  <CardBasic class="flex flex-row gap-x-4">
     <TextAreaBasic
       v-model="text"
-      class="textarea"
+      class="flex-auto"
       :class="{ ...fontClass }"
       :autosize="{ minRows: 2, maxRows: 4 }"
       placeholder="输入内容"
     >
       {{ text }}
     </TextAreaBasic>
+    <div class="action-wrapper flex-none">
+      <SvgIcon
+        class="action-button"
+        :color="variables.color_clear_btn"
+        :icon-src="ImgClose"
+        @click="clear()"
+      >
+      </SvgIcon>
+    </div>
   </CardBasic>
 </template>
+
+<style scoped lang="scss">
+@use "../assets/action-wrapper.scss";
+@include action-wrapper.action-wrapper;
+</style>
