6a4d1c5b7fb5c2a0f2451e3fce28c3a04b12e31d
Alex Fang <deeno@qq.com>
2023-05-10 19:27:23 +0800

    feat(font-selector): add font selector panel

--- src/views/pageTypewriter/PageTypewriter.vue
+++ src/views/pageTypewriter/PageTypewriter.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { useFont } from "./hooks";
 import TransformSwitcher from "./components/Portrait/TransformSwitcher/TransformSwitcher.vue";
+import FontSelector from "./components/Portrait/FontSelector/FontSelector.vue";
 import InputArea from "./components/Portrait/InputArea/InputArea.vue";
 import PreviewArea from "./components/Portrait/PreviewArea/PreviewArea.vue";
 import KeyboardLayout from "./components/Portrait/KeyboardLayout/KeyboardLayout.vue";
-
-import ActionBar from "./components/ActionBar.vue";
 
 const { installFonts } = useFont();
 
@@ -14,12 +13,11 @@ installFonts();
 
 <template>
   <main class="main-container flex flex-col fixed inset-0 gap-y-3 px-5 py-4">
-    <TransformSwitcher class="flex-none mb-3" />
+    <TransformSwitcher class="flex-none" />
+    <FontSelector class="flex-none" />
     <InputArea class="flex-none" />
     <PreviewArea class="flex-auto" />
     <KeyboardLayout class="flex-none" />
-
-    <ActionBar class="flex-none" />
   </main>
 </template>
 
---
+++ src/views/pageTypewriter/components/Portrait/FontSelector/FontSelector.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import type { FontNode } from "@/shared";
+import { useConfig, useFont, useFontSelector } from "../../../hooks";
+import variables from "./variables.module.scss";
+import CardBasic from "@/components/CardBasic.vue";
+import SvgIcon from "@/components/SvgIcon.vue";
+
+const wrapperRef = ref<HTMLDivElement | null>(null);
+
+const { config } = useConfig();
+const { fontOptions } = useFont();
+const { selectorVisible, closeSelector } = useFontSelector();
+
+const dialogStyle = computed(() => {
+  return {
+    maxHeight: `calc(100vh - ${wrapperRef.value?.offsetTop}px - ${variables.dialog_span_bottom} - ${variables.dialog_padding_y} * 2)`,
+  };
+});
+
+const changeFont = (fontOption: FontNode) => {
+  config.value.font = fontOption;
+  closeSelector();
+};
+</script>
+
+<template>
+  <div ref="wrapperRef" class="selector-wrapper">
+    <CardBasic v-if="selectorVisible" class="selector-dialog">
+      <div class="content scrollbar" :style="{ ...dialogStyle }">
+        <div
+          v-for="(fontGroup, groupIndex) in fontOptions"
+          :key="groupIndex"
+          class="group flex flex-row"
+        >
+          <SvgIcon
+            class="icon flex-none"
+            :color="variables.dialog_icon_color"
+            :icon-src="fontGroup.icon!"
+          />
+          <div class="list">
+            <div
+              v-for="(fontItem, itemIndex) in fontGroup.children"
+              :key="itemIndex"
+              class="item cursor-pointer"
+              :class="{ active: fontItem.tag === config.font.tag }"
+              @click="changeFont(fontItem)"
+            >
+              {{ fontItem.abbr || fontItem.label }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </CardBasic>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "sass:math";
+@use "@/assets/vars/color.scss" as *;
+@use "@/assets/effects/scrollbar.scss";
+@use "./dialog.scss" as *;
+@include scrollbar.scrollbar;
+
+$list-span: 4.44rem;
+$list-line-height: 3.24rem;
+$list-font-size: 1.6rem;
+$list-logo-width: 9.44rem;
+
+.selector-wrapper {
+  position: relative;
+}
+
+.selector-dialog {
+  position: absolute;
+  width: 100%;
+  z-index: 100000000;
+  padding: $dialog-padding-y $dialog-padding-x !important;
+
+  .content {
+    overflow-y: auto;
+
+    .group {
+      column-gap: $list-span;
+      align-items: start;
+
+      & + .group {
+        border-top: 0.1rem solid $color-gray-6;
+      }
+
+      .icon {
+        width: $list-logo-width;
+      }
+
+      .list {
+        .item {
+          font-size: $list-font-size;
+          line-height: $list-line-height;
+          color: $color-gray-2;
+          &.active {
+            color: $color-primary-1;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
