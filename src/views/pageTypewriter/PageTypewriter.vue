fb071ed98464edb62baa85bed3d072a2b0e0f923
Alex Fang <deeno@qq.com>
2023-05-19 21:22:04 +0800

    feat(portrait-layout): make portrait layout to separate component with theme friendly styles

--- src/views/pageTypewriter/PageTypewriter.vue
+++ src/views/pageTypewriter/PageTypewriter.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
 import { useFont } from "./hooks";
-import TransformSwitcher from "./components/Portrait/TransformSwitcher/TransformSwitcher.vue";
-import FontSelector from "./components/Portrait/FontSelector/FontSelector.vue";
-import InputArea from "./components/Portrait/InputArea/InputArea.vue";
-import PreviewArea from "./components/Portrait/PreviewArea/PreviewArea.vue";
-import KeyboardLayout from "./components/Portrait/KeyboardLayout/KeyboardLayout.vue";
-import ActionBar from "./components/Portrait/ActionBar/ActionBar.vue";
+import PortraitLayout from "./components/Portrait/PortraitLayout/PortraitLayout.vue";
 
 const { installFonts } = useFont();
 
@@ -13,20 +8,5 @@ installFonts();
 </script>
 
 <template>
-  <main class="main-container flex flex-col fixed inset-0 gap-y-3 px-5 py-4">
-    <TransformSwitcher class="flex-none" />
-    <FontSelector class="flex-none" />
-    <InputArea class="flex-none" />
-    <PreviewArea class="flex-auto" />
-    <KeyboardLayout class="flex-none" />
-    <ActionBar class="flex-none" />
-  </main>
+  <PortraitLayout />
 </template>
-
-<style scoped lang="scss">
-@use "@/assets/vars/color.scss" as *;
-
-.main-container {
-  background-color: $color-bg;
-}
-</style>
---
+++ src/views/pageTypewriter/components/Portrait/PortraitLayout/PortraitLayout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import TransformSwitcher from "../TransformSwitcher/TransformSwitcher.vue";
+import FontSelector from "../FontSelector/FontSelector.vue";
+import InputArea from "../InputArea/InputArea.vue";
+import PreviewArea from "../PreviewArea/PreviewArea.vue";
+import KeyboardLayout from "../KeyboardLayout/KeyboardLayout.vue";
+import ActionBar from "../ActionBar/ActionBar.vue";
+</script>
+
+<template>
+  <main class="main-container flex flex-col fixed inset-0 gap-y-3 px-5 py-4">
+    <TransformSwitcher class="flex-none" />
+    <FontSelector class="flex-none" />
+    <InputArea class="flex-none" />
+    <PreviewArea class="flex-auto" />
+    <KeyboardLayout class="flex-none" />
+    <ActionBar class="flex-none" />
+  </main>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/effects/theme.scss";
+@use "./color.scss" as *;
+
+@include theme.themeify($colors) {
+  .main-container {
+    background-color: theme.t("layout-bg-color");
+  }
+}
+</style>
