c72aa27cde7798def862afd927fc62bfdea99be0
Alex Fang <deeno@qq.com>
2024-03-31 13:40:21 +0800

    build(download-font): move release id to font cache base folder

--- build/download-fonts/env.ts
+++ build/download-fonts/env.ts
@@ -4,7 +4,7 @@ import Path from "node:path";
 export const srcBasePath: string = Path.resolve(__dirname, "../../src/");
 export const latestReleaseIdPath: string = Path.resolve(
   __dirname,
-  "./release-id.txt",
+  "./.cache/release-id.txt",
 );
 export const fontCacheBasePath: string = Path.resolve(__dirname, "./.cache/");
 export const fontDownloadBasePath: string = Path.resolve(
